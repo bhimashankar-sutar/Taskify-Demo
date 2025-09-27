7df94aaf4a2b7d3105cede24534317d16f63afe6
unknown <kulameay@gmail.com>
2025-09-27 23:22:59 +0530

    add initial files

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './style/navstyle.css';
+function Navbar(){
+    return(
+        <>
+        <nav>
+            <div className="nav-items-left">
+                <h4 className="item">Taskify</h4>
+                <h6 className="item">Projects</h6>
+                <h6 className="item">People</h6>
+                <button  className="item-create"type="button">Create</button>
+            </div>
+            
+        </nav>
+        <div>
+            "Work in progress..."
+        </div>
+        </>
+        
+    )
+}
+
+export default Navbar;
